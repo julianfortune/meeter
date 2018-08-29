ddb0667917fb68842a2f9bab89cf8095bbfda359
Julian Fortune <juliandfortune@gmail.com>
2018-08-28 20:20:19 -0700

    Initial files

---
+++ Meeter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Meeter
+//
+//  Created by Julian Fortune on 8/28/18.
+//  Copyright © 2018 Julian Fortune. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+}
+
